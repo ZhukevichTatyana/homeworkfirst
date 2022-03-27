0c312dc475f9ae3351cace2214fa74f8cc5c5db9
ZhukevichTatyana <90384180+ZhukevichTatyana@users.noreply.github.com>
2022-03-27 15:57:47 +0300

    Add files via upload

--- homework/src/js/script.js
+++ homework/src/js/script.js
@@ -1,3 +1,39 @@
-let userName= prompt ("What is Your name?");
-alert ("Hello, "+ userName +"! How are you?");
+//homework1
+// let userName= prompt ("What is Your name?");
+// alert ("Hello, "+ userName +"! How are you?");
+
+//homework2
+//first variant
+
+// const n = prompt ('Выберете действие +, -, / , *');
+// const a = +prompt ('Напишите число');
+// const b = +prompt ('Напишите второе число');
+// if (n === "+" ) {
+//     alert (`${a} + ${b} = ${a+b}`);
+// } else if (n === "-") {
+//     alert (`${a} - ${b} = ${a-b}`);
+// }else if (n === "*") {
+//     alert(`${a} * ${b} = ${a*b}`);
+// }else {(n === "/") 
+//     alert(`${a} / ${b} = ${a/b}`);
+// }
+
+
+// second variant
+
+const n = prompt ('Выберете действие +, -, / , *');
+const a = +prompt ('Напишите число');
+const b = +prompt ('Напишите второе число');
+
+
+switch (n) {
+    case "+": alert (`${a} + ${b} = ${a+b}`); break;
+    case "-": alert (`${a} - ${b} = ${a-b}`); break;
+    case "*": alert (`${a} * ${b} = ${a*b}`); break;
+    case "/": alert (`${a} / ${b} = ${a/b}`); break;
+}
+
+
+
+
 
