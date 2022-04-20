3da8c56a465151b74ea1fec277d07b87bbec8b7d
Tania <swaggietan@gmail.com>
2022-04-20 20:00:09 +0300

    homework8

---
+++ homework7/js/script.js
@@ -0,0 +1,24 @@
+// "use strict"
+
+// function createCalculator (y) {
+// return {
+//   sum: (x) => y += x,
+//   mult: (x) => y *= x,
+//   sub:(x) => y -= x,
+//   div:(x) => y /= x,
+//   set:(x) => y = x
+// } 
+// }
+// const calc = createCalculator(10);
+//  console.log(calc.sum(5))/// 15
+//  console.log(calc.mult(10)); // 150
+//  console.log(calc.sub(40)); // 110
+//  console.log(calc.div(10)); // 11
+//  console.log(calc.set(100)); // 100
+//  console.log(calc.sum(5))
+//  console.log(calc.mult(10)); 
+//  console.log(calc.sub(40)); 
+//  console.log(calc.div(10)); 
+const a = 1;
+const b = 1;
+console.log ( a == b);
