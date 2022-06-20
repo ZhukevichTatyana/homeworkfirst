0ba6d8357e114169a40413ba94545be6e64c765f
ZhukevichTatyana <90384180+ZhukevichTatyana@users.noreply.github.com>
2022-06-20 16:33:56 +0300

    Add files via upload

---
+++ homework/dz24-webpack/webpack.config.js
@@ -0,0 +1,26 @@
+
+  const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader'],
+      },
+    ],
+  },
+  // devServer: {
+  //   static: {
+  //     directory: path.join(__dirname, 'dist'),
+  //   },
+  //   compress: true,
+  //   port: 9000,
+  // }
+};
